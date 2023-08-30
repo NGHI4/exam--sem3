b1ea43b942d920b123f1cffe76440fd7032b7725
NGHI4 <110522520+NGHI4@users.noreply.github.com>
2023-08-30 15:47:07 +0700

    nop bai

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,79 @@
+const Product = require("./../models/product.model");
+const Category = require("./../models/category.model");
+const fs = require("fs");
+exports.list = async (req,res)=>{
+    try {
+        const rs = await Product.find().populate('category').exec();
+        res.render("product/list",{products:rs});
+    } catch (error) {
+        
+    }
+}
+exports.formCreate = (req,res)=>{
+    const data = req.body;
+    
+    data.url = req._parsedOriginalUrl.path;
+    res.render("product/form",{product:data});
+}
+exports.store = async (req,res)=>{
+    const data = req.body;
+    const file = req.file;
+    if(file){
+        const img = fs.readFileSync(file.path);
+        data.thumbnail = {
+            contentType: file.mimetype,
+            data:img.toString("base64")
+        }
+    }
+    try {
+        const p = new Product(data);
+        await p.save();
+        res.redirect("/product");
+    } catch (error) {
+        res.render("product/form",{product:data,error:error});
+    }
+}
+
+exports.formEdit = async (req,res)=>{
+    const _id = req.params.id;
+    try {
+        const product = await Product.findById(_id).populate("category").exec();
+        product.url = req._parsedOriginalUrl.path;
+        res.render("product/form",{product:product});
+    } catch (error) {
+        res.send(error);
+    }
+    
+}
+
+exports.update = async (req,res)=>{
+    const _id = req.params.id;
+    const data= req.body;
+    const product = await Product.findById(_id);
+    try {
+        const file = req.file;
+        if(file){
+            const img = fs.readFileSync(file.path);
+            data.thumbnail = {
+                contentType: file.mimetype,
+                data:img.toString("base64")
+            }
+        }else{
+            data.thumbnail = product.thumbnail;
+        }
+        await Product.findByIdAndUpdate(_id,data);
+        res.redirect("/product");
+    } catch (error) {
+        res.render("product/form",{product:product});
+    }
+}
+
+exports.delete =  async (req,res)=>{
+    const _id = req.params.id;
+    try {
+        await Product.findByIdAndDelete(_id);
+        res.redirect("/product");
+    } catch (error) {
+        res.redirect("/product");
+    }
+}
